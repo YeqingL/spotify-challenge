b096bdd549227dc0681365bf0efbd92b415c28c1
YeqingL <qq921027@hotmail.com>
2015-07-10 12:51:06 -0700

    Added content

---
+++ app.js
@@ -0,0 +1,56 @@
+var app = angular.module("myApp", []);
+
+var ctrl = app.controller("myCtrl", function($scope, $http) {
+    $scope.selectedType = $("#select").val();
+    $scope.keyword = $('input').val();
+    $scope.audioObject = {};
+    $scope.dataInfo = false;
+    $scope.displayInfo = 'hide';
+
+    $("#select").change(function() {
+        $scope.selectedType = $(this).val();
+    });
+
+    $scope.getData = function() {
+        $http.get("https://api.spotify.com/v1/search?type=" + $scope.selectedType + "&query=" + $scope.keyword)
+             .success(function(response) {
+                if ($scope.selectedType == "album" && response.albums.items.length > 0) {
+                    data = $scope.currentType = [response.albums.items[0],response.albums.items[1],response.albums.items[2]];
+                } else if ($scope.selectedType == "artist") {
+                    data = $scope.currentType = response.artists.items;
+                } else if ($scope.selectedType == "playlist") {
+                    data = $scope.currentType = response.playlists.items;
+                } else if ($scope.selectedType == "track") {
+                    data = $scope.currentType = response.tracks.items;
+                } else {
+                    $scope.currentType = [];
+                }
+             });
+    };
+
+    $scope.toggle = function() {
+        $scope.dataInfo = !$scope.dataInfo;
+        if (!$scope.dataInfo) {
+            $scope.displayInfo = 'show';
+        } else {
+            $scope.displayInfo = 'hide';
+        }
+    };
+
+    $scope.play = function(song) {
+        if($scope.currentSong == song) {
+            $scope.audioObject.pause();
+            $scope.currentSong = false;
+            return;
+        } else {
+            if($scope.audioObject.pause != undefined) $scope.audioObject.pause();
+            $scope.audioObject = new Audio(song);
+            $scope.audioObject.play();  
+            $scope.currentSong = song;
+        }
+    };
+});
+
+$('body').tooltip({
+    selector: '[title]'
+});
